27233a38fa503b1a30dc50bbbd09076b72305890
QuentinDess <quentin.dessartine@gmail.com>
2021-05-17 16:54:20 +0200

    first commit

---
+++ src/Controller/HobbyController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HobbyController extends AbstractController
+{
+    /**
+     * @Route("/hobby", name="hobby")
+     */
+    public function index(): Response
+    {
+        return $this->render('hobby/index.html.twig', [
+            'controller_name' => 'HobbyController',
+        ]);
+    }
+}
